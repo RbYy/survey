92fb3d34cae25b342c3e1203313aa11e04aec531
RbYy <robert.brumat@gmail.com>
2016-10-11 14:04:07 +0200

    add dj-database-url to requirements.txt. update settings

--- pollproject/settings.py
+++ pollproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -144,3 +145,7 @@ DATABASES = {
     }
 }
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
